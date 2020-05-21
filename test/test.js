5b3787f9fdfb0016c83a787d63189730fc2c81d1
du.j@trip.com <sweety_juaner@163.com>
2020-05-21 13:30:43 +0800

    update

---
+++ test/test.js
@@ -0,0 +1,33 @@
+var head = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: {
+          value: 5,
+          next: null,
+        },
+      },
+    },
+  },
+};
+
+function findIndexofK(head, k) {
+  if (!head) return null;
+  let p1 = head;
+  let p2 = head;
+  while (k) {
+    p1 = p1.next;
+    k--;
+  }
+  while (p1) {
+    p1 = p1.next;
+    p2 = p2.next;
+  }
+  return p2;
+}
+
+console.log(findIndexofK(head, 2));
