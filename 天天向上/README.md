9e17ba34d8549621d9bebd20d5e1f8537f094aba
du.j@trip.com <sweety_juaner@163.com>
2020-07-17 15:51:43 +0800

    daydayup

---
+++ 天天向上/README.md
@@ -0,0 +1,39 @@
+# DAY DAY UP
+
+从 2020.07.17 开始记录每天要学习内容的内容，可以方便回顾
+
+2020.07.17
+
+算法
+
+- leetcode104：二叉树的最大深度
+- leetcode236：二叉树的最近公共祖先
+- Offer&leetcode110：平衡二叉树
+- leetcode112：路径总和
+- Offer&leetcode101：对称二叉树
+- 字节一面：给定一个二叉树, 找到该树中两个指定节点间的最短距离
+
+待学习
+
+1. ssr 构建
+2. webpack 源码整理和性能优化
+3. 性能优化
+   雅虎军规
+4. 算法
+
+旋转数组
+
+实战：
+1、ts 实战
+2、hook 实战
+3、ssr 实战
+
+应用的部署
+
+docker
+graphQL
+
+学习：
+app 端：taro 跨端、rn，weex，flutter
+electron: 使用 zoomsdk
+wepack：hmr 原理
--- 工具类/对象的链式属性.js
+++ 工具类/对象的链式属性.js
@@ -1,15 +1,15 @@
-var result = "a.b.c?.x".replace(new RegExp(/([\$_\w\.]+\?\.)/, "g"), function (
-  res
-) {
-  let str = res.replace(/\?\./, "");
-  let arrs = str.split(".");
-  let strArr = [];
-  for (let i = 1; i <= arrs.length; i++) {
-    strArr.push(arrs.slice(0, i).join("."));
-  }
-  let compile = strArr.join("&&");
-  const done = compile + "&&" + str + ".";
-  return done;
-});
+function format(orgin) {
+  return orgin.replace(new RegExp(/([\$_\w\.]+\?\.)/, "g"), function (res) {
+    let str = res.replace(/\?\./, "");
+    let arrs = str.split(".");
+    let strArr = [];
+    for (let i = 1; i <= arrs.length; i++) {
+      strArr.push(arrs.slice(0, i).join("."));
+    }
+    let compile = strArr.join("&&");
+    const done = compile + "&&" + str + ".";
+    return done;
+  });
+}
 
-console.log(result); //a&&a.b&&a.b.c&&a.b.c.x
+console.log(format("a.b.c?.x")); //a&&a.b&&a.b.c&&a.b.c.x
