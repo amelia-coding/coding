fb117f29593b5753666190e95a48094ed7503027
du.j@trip.com <sweety_juaner@163.com>
2020-07-23 14:44:04 +0800

    up

--- 算法/offer/59 - I. 滑动窗口的最大值.js
+++ 算法/offer/59 - I. 滑动窗口的最大值.js
@@ -29,3 +29,43 @@ var maxSlidingWindow = function (nums, k) {
 };
 
 maxSlidingWindow([9, 10, 9, -7, -4, -8, 2, -6], 5);
+
+/*优雅*/
+
+var maxSlidingWindow = function (nums, k) {
+  let n = nums.length;
+  let res = [];
+  let windows = new slideWindow();
+  for (let i = 0; i < n; i++) {
+    if (i < k - 1) {
+      windows.push(nums[i]);
+    } else {
+      windows.push(nums[i]);
+      res.push(windows.max());
+      windows.pop(nums[i - k + 1]);
+    }
+  }
+  return res;
+};
+
+class slideWindow {
+  constructor() {
+    this.data = [];
+  }
+  push(val) {
+    let data = this.data;
+    while (data.length > 0 && data[data.length - 1] < val) {
+      data.pop();
+    }
+    data.push(val);
+  }
+  pop(val) {
+    let data = this.data;
+    if (data.length > 0 && data[0] === val) {
+      data.shift();
+    }
+  }
+  max() {
+    return this.data[0];
+  }
+}
