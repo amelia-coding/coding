85cb89d333c8b158e979816a7a39715ce411eaa4
du.j@trip.com <sweety_juaner@163.com>
2020-07-23 14:26:39 +0800

    归并

---
+++ 算法/sort/mergeSort.js
@@ -0,0 +1,31 @@
+/**
+ * 归并排序
+ * @param {} arr
+ */
+function mergeSort(arr) {
+  //采用自上而下的递归方法
+  const len = arr.length;
+  if (len < 2) {
+    return arr;
+  }
+  // length >> 1 和 Math.floor(len / 2) 等价
+  let middle = Math.floor(len / 2),
+    left = arr.slice(0, middle),
+    right = arr.slice(middle); // 拆分为两个子数组
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+const merge = (left, right) => {
+  const result = [];
+  while (left.length && right.length) {
+    // 注意: 判断的条件是小于或等于，如果只是小于，那么排序将不稳定.
+    if (left[0] <= right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+  while (left.length) result.push(left.shift());
+  while (right.length) result.push(right.shift());
+  return result;
+};
