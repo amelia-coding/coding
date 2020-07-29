6e112528948bd80069d24d65d1daf3c2d98c0618
du.j@trip.com <sweety_juaner@163.com>
2020-07-29 20:18:13 +0800

    feat eventEmmiter

--- 工具类/对象的链式属性.js
+++ 工具类/对象的链式属性.js
@@ -2,9 +2,11 @@ function format(orgin) {
   return orgin.replace(new RegExp(/([\$_\w\.]+\?\.)/, "g"), function (res) {
     let str = res.replace(/\?\./, "");
     let arrs = str.split(".");
+    console.log(str, arrs);
     let strArr = [];
     for (let i = 1; i <= arrs.length; i++) {
       strArr.push(arrs.slice(0, i).join("."));
+      console.log(strArr);
     }
     let compile = strArr.join("&&");
     const done = compile + "&&" + str + ".";
