3bfcbd8d3a25396dcb23159fd7b7ead0729eb93c
du.j@trip.com <sweety_juaner@163.com>
2020-07-30 18:09:28 +0800

    commit

--- 算法/offer/59 - I. 滑动窗口的最大值.js
+++ 算法/offer/59 - I. 滑动窗口的最大值.js
@@ -1,37 +1,3 @@
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {number[]}
- */
-var maxSlidingWindow = function (nums, k) {
-  if (nums.length === 0) return [];
-  let queue = [];
-  let res = [];
-  for (let i = 0; i < k; i++) {
-    while (queue.length > 0 && queue[queue.length - 1] < nums[i]) {
-      queue.pop();
-    }
-    queue.push(nums[i]);
-  }
-  res.push(queue[0]);
-
-  for (let i = k; i < nums.length; i++) {
-    while (queue.length > 0 && queue[queue.length - 1] < nums[i]) {
-      queue.pop();
-    }
-    if (nums[i - k] === queue[0]) queue.shift(); //当前左窗口的值等于队头元素时被踢出
-    queue.push(nums[i]);
-    res.push(queue[0]);
-  }
-
-  console.log(res);
-  return res;
-};
-
-maxSlidingWindow([9, 10, 9, -7, -4, -8, 2, -6], 5);
-
-/*优雅*/
-
 var maxSlidingWindow = function (nums, k) {
   let n = nums.length;
   let res = [];
@@ -50,7 +16,7 @@ var maxSlidingWindow = function (nums, k) {
 
 class slideWindow {
   constructor() {
-    this.data = [];
+    this.data = []; //单调队列
   }
   push(val) {
     let data = this.data;
