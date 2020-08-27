484af4ab7ebca7a9def8a3ea6d7c21d6f472bdd0
du.j@trip.com <sweety_juaner@163.com>
2020-08-27 10:55:11 +0800

    update

---
+++ 算法/leetcode/56. 合并区间.js
@@ -0,0 +1,45 @@
+/*
+56. 合并区间
+给出一个区间的集合，请合并所有重叠的区间。
+
+ 
+
+示例 1:
+
+输入: intervals = [[1,3],[2,6],[8,10],[15,18]]
+输出: [[1,6],[8,10],[15,18]]
+解释: 区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+示例 2:
+
+输入: intervals = [[1,4],[4,5]]
+输出: [[1,5]]
+解释: 区间 [1,4] 和 [4,5] 可被视为重叠区间。
+注意：输入类型已于2019年4月15日更改。 请重置默认代码定义以获取新方法签名。
+*/
+
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+let merge = (intervals) => {
+  const len = intervals.length;
+  if (len === 0) return [];
+  // 排序
+  intervals.sort((s1, s2) => {
+    return s1[0] - s2[0];
+  });
+  let arr = [];
+  arr.push(intervals[0]);
+  for (let i = 1; i < len; i++) {
+    const arrLen = arr.length;
+    if (intervals[i][0] <= arr[arrLen - 1][1]) {
+      arr[arrLen - 1][1] =
+        intervals[i][1] > arr[arrLen - 1][1]
+          ? intervals[i][1]
+          : arr[arrLen - 1][1];
+    } else {
+      arr.push(intervals[i]);
+    }
+  }
+  return arr;
+};
