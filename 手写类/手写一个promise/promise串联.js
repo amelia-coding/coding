eb9ce1fb3fc532900bf65c663f7223954e1c6a59
du.j@trip.com <sweety_juaner@163.com>
2020-09-10 14:08:28 +0800

    update

---
+++ 手写类/手写一个promise/promise串联.js
@@ -0,0 +1,25 @@
+var createPromise = function (time) {
+  return () =>
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        console.log("timein" + time);
+        resolve();
+      }, time * 1000);
+    });
+};
+
+function serpromise(arr) {
+  arr.reduce((pre, next, index, carr) => {
+    return pre.then(next); //本质就是不停地调用then
+  }, Promise.resolve());
+}
+
+var arr = [
+  createPromise(2),
+  createPromise(1),
+  createPromise(3),
+  createPromise(4),
+  createPromise(5),
+];
+// Promise.resolve().then(createPromise(2)).then(createPromise(1))
+serpromise(arr);
