62e9531d01c9b72d1b21a6b4011f914033fb57ee
dujuan <dujuan@bixin.cn>
2020-11-20 14:34:29 +0800

    fix

--- ！补充题 42.webp优化方案/libwebp/webp_js/CMakeFiles/webp.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libwebp.a"
-  "libwebp.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/webp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
