dd5bfacb10384b0f7413fad70bb19d93bdfaaace
dujuan <dujuan@bixin.cn>
2021-01-04 00:29:37 +0800

    update

---
+++ 718. 最长重复子数组.js
@@ -0,0 +1,112 @@
+/*
+718. 最长重复子数组
+给两个整数数组 A 和 B ，返回两个数组中公共的、长度最长的子数组的长度。
+
+ 
+
+示例：
+
+输入：
+A: [1,2,3,2,1]
+B: [3,2,1,4,7]
+输出：3
+解释：
+长度最长的公共子数组是 [3, 2, 1] 。
+*/
+
+/**
+ * 滑动窗口
+ * @param {number[]} A
+ * @param {number[]} B
+ * @return {number}
+ */
+const findLength = (A, B) => {
+  let aLength = A.length,
+    bLength = B.length
+  //total是总共运行的次数
+  let total = aLength + bLength - 1
+  let max = 0
+  for (let i = 0; i < total; i++) {
+    //下面一大坨主要判断数组A和数组B比较的起始位置和比较的长度
+    let aStart = 0
+    let bStart = 0
+    let len = 0
+    if (i < aLength) {
+      aStart = aLength - i - 1
+      bStart = 0
+      len = i + 1
+    } else {
+      aStart = 0
+      bStart = i - aLength + 1
+      len = Math.min(bLength - bStart, aLength)
+    }
+    let maxlen = maxLen(A, B, aStart, bStart, len)
+    max = Math.max(max, maxlen)
+  }
+  return max
+}
+
+function maxLen(A, B, aStart, bStart, len) {
+  let max = 0,
+    count = 0
+  for (let i = 0; i < len; i++) {
+    if (A[aStart + i] == B[bStart + i]) {
+      count++
+      max = Math.max(max, count)
+    } else {
+      count = 0
+    }
+  }
+  return max
+}
+
+/**
+ * 动态规划
+ * @param {number[]} A
+ * @param {number[]} B
+ * @return {number}
+ */
+
+const findLength = (A, B) => {
+  const m = A.length
+  const n = B.length
+  const dp = new Array(m + 1)
+  for (let i = 0; i <= m; i++) {
+    // 初始化整个dp矩阵，每个值为0
+    dp[i] = new Array(n + 1).fill(0)
+  }
+  let res = 0
+  // i=0或j=0的base case，初始化时已经包括
+  for (let i = 1; i <= m; i++) {
+    // 从1开始遍历
+    for (let j = 1; j <= n; j++) {
+      // 从1开始遍历
+      if (A[i - 1] == B[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1
+      } // A[i-1]!=B[j-1]的情况，初始化时已包括了
+      res = Math.max(dp[i][j], res)
+    }
+  }
+  return res
+}
+
+//降维
+const findLength = (A, B) => {
+  const m = A.length
+  const n = B.length
+  const dp = new Array(n + 1).fill(0)
+  let res = 0
+  for (let i = 1; i <= m; i++) {
+    for (let j = n; j >= 1; j--) {
+      if (A[i - 1] == B[j - 1]) {
+        dp[j] = dp[j - 1] + 1
+      } else {
+        dp[j] = 0
+      }
+
+      res = Math.max(dp[j], res)
+    }
+    console.log(dp)
+  }
+  return res
+}
---
+++ 729. 我的日程安排表 I.js
@@ -0,0 +1,71 @@
+/*
+729. 我的日程安排表 I
+实现一个 MyCalendar 类来存放你的日程安排。如果要添加的时间内没有其他安排，则可以存储这个新的日程安排。
+
+MyCalendar 有一个 book(int start, int end)方法。它意味着在 start 到 end 时间内增加一个日程安排，注意，这里的时间是半开区间，即 [start, end), 实数 x 的范围为，  start <= x < end。
+
+当两个日程安排有一些时间上的交叉时（例如两个日程安排都在同一时间内），就会产生重复预订。
+
+每次调用 MyCalendar.book方法时，如果可以将日程安排成功添加到日历中而不会导致重复预订，返回 true。否则，返回 false 并且不要将该日程安排添加到日历中。
+
+请按照以下步骤调用 MyCalendar 类: MyCalendar cal = new MyCalendar(); MyCalendar.book(start, end)
+
+示例 1:
+
+MyCalendar();
+MyCalendar.book(10, 20); // returns true
+MyCalendar.book(15, 25); // returns false
+MyCalendar.book(20, 30); // returns true
+解释: 
+第一个日程安排可以添加到日历中.  第二个日程安排不能添加到日历中，因为时间 15 已经被第一个日程安排预定了。
+第三个日程安排可以添加到日历中，因为第一个日程安排并不包含时间 20 。
+*/
+
+var MyCalendar = function () {
+  this.calendar = []
+}
+
+/**
+ * @param {number} start
+ * @param {number} end
+ * @return {boolean}
+ */
+MyCalendar.prototype.book = function (start, end) {
+  for (let iv of this.calendar) {
+    if (iv[0] < end && iv[1] > start) return false
+  }
+  this.calendar.push([start, end])
+  return true
+}
+
+/**
+ * 二分
+ * Your MyCalendar object will be instantiated and called as such:
+ * var obj = new MyCalendar()
+ * var param_1 = obj.book(start,end)
+ */
+
+var MyCalendar = function () {
+  this.cal = []
+}
+
+/**
+ * @param {number} start
+ * @param {number} end
+ * @return {boolean}
+ */
+MyCalendar.prototype.book = function (start, end) {
+  let left = 0
+  let right = this.cal.length
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2)
+    const [e1, e2] = this.cal[mid]
+    if (start >= e2) {
+      left = mid + 1
+    } else if (end <= e1) {
+      right = mid
+    } else return false
+  }
+  this.cal.splice(left, 0, [start, end])
+  return true
+}
