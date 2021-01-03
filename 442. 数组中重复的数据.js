0376e1c787720f7648caedfc314da8fbaa784456
dujuan <dujuan@bixin.cn>
2021-01-03 15:31:31 +0800

    update

---
+++ 442. 数组中重复的数据.js
@@ -0,0 +1,43 @@
+/*
+给定一个整数数组 a，其中1 ≤ a[i] ≤ n （n为数组长度）, 其中有些元素出现两次而其他元素出现一次。
+
+找到所有出现两次的元素。
+
+你可以不用到任何额外空间并在O(n)时间复杂度内解决这个问题吗？
+
+示例：
+
+输入:
+[4,3,2,7,8,2,3,1]
+
+输出:
+[2,3]
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDuplicates = function (nums) {
+  nums.sort((a, b) => a - b)
+  //只适用元素出现2次
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== nums[i + 1]) {
+      nums.splice(i, 1)
+      i--
+    } else {
+      nums.splice(i, 1)
+    }
+  }
+  return nums
+}
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDuplicates = function (nums) {
+  nums.sort((x, y) => x - y)
+  nums = nums.filter((v, i) => v == nums[i - 1])
+  return nums
+}
