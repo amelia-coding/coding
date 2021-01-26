cd1bcd136a760dda5e5cdba3dd79b8ccd053678f
dujuan <dujuan@bixin.cn>
2021-01-26 15:21:57 +0800

    up

--- <html>.js
+++ <html>.js
@@ -1,10 +1,36 @@
-<html>
-  <head></head>
-  <body>
-    <div>
-        <span>f</span>
-        <span>o</span>
-        <span>o</span>
-    </div>
-  </body>
-</html>
+function reverse (head, k) {
+  let [pre, cur] = [null, head]
+  // 判断是否
+  let p = head
+  for (let i = 0; i < k; i++) {
+    if (p === null) return head
+    p = p.next
+  }
+  // 反转
+  for (let i = 0; i < k; i++) {
+    const temp = cur.next
+    cur.next = pre
+    pre = cur
+    cur = temp
+  }
+  head.next = reverse(cur, k)
+  return pre
+}
+
+function ListNode (val) {
+  this.val = val
+  this.next = null
+}
+function ger (arr) {
+  let head = new ListNode(arr[0])
+  const p = head
+  for (let i = 1; i < arr.length; i++) {
+    head.next = new ListNode(arr[i])
+    head = head.next
+  }
+  return p
+}
+
+const lsit = ger([1, 2, 3, 4, 5])
+console.log(JSON.stringify(lsit))
+console.log(JSON.stringify(reverse(lsit, 2)))
---
+++ ！补充题 49.拆红包.js
@@ -0,0 +1,19 @@
+//m块钱随机分给n个人,要求每人得到的金额大于 0.01
+
+//1.定义一个random()函数，原理是 随机数和最大值减最小值的差相乘 最后再加上最小值。
+function random(min, max) {
+  return (Math.random() * (max - min) + min).toFixed(2)
+}
+//2.拆红包
+function allocate(m, n) {
+  if (m / n < 0.01) return
+  let res = []
+  for (let i = 0; i < n; i++) {
+    res[i] = random(0.01, (m / n) * 2) //当前分配红包的范围是0.01 - 剩余金额平均值*2
+    m = (m - res[i]).toFixed(2)
+    console.log(`第${i + 1}人分配: ${res[i]},剩余: ${m}`)
+  }
+  return res
+}
+
+allocate(1, 10)
