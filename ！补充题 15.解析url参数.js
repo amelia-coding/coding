b65740146b1a3ff42cc2517ac2c96b6e281bba60
dujuan <dujuan@bixin.cn>
2021-01-29 00:17:26 +0800

    update

--- ！补充题 15.解析url参数.js
+++ ！补充题 15.解析url参数.js
@@ -1,21 +1,14 @@
 const parseQueryString = (url) => {
-  if (typeof url === "string") {
-    let obj = {};
-    const paramPart = url.replace(/#\S*/, "").match(/\?[^#]+/);
-    if (paramPart === null) {
-      return {};
-    }
-    const paramGroup = paramPart[0].substr(1).split("&");
-    for (let i = 0, length = paramGroup.length; i < length; i++) {
-      let name = paramGroup[i].split("=")[0];
-      let value = paramGroup[i].match(/[=].*/)
-        ? paramGroup[i].match(/[=].*/)[0].substr(1)
-        : null;
-      obj[name] = value;
-    }
-
-    return obj;
-  } else {
-    throw new Error("请传入字符串");
+  if (typeof url !== 'string') throw new Error('请传入字符串')
+  let query = {}
+  let param = url.replace(/#\S*/, '').match(/\?[^#]+/)//去除#
+  if (param === null) return {}
+  param = param[0].substr(1).split('&')
+  for (let i = 0, length = param.length; i < length; i++) {
+    let [name, value] = param[i].split('=')
+    query[name] = value
   }
-};
+  return query
+}
+
+parseQueryString('com?a=1&b=48')
