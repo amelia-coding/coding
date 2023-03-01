2db2d9f6ff607a74aa57c2310a69bdba44aad1e6
dujuan <dujuan@bixin.cn>
2023-03-01 15:22:38 +0800

    feat: 二叉树

--- 102.二叉树的层序遍历.js
+++ 102.二叉树的层序遍历.js
@@ -50,3 +50,57 @@ const levelOrder = function (root) {
   }
   return res;
 };
+
+
+function bfs(root, key = 'name') {
+  if (root === null) return []
+  const q = [root]
+  const res = []
+  while (q.length > 0) {
+    let cnt = q.length
+    const temp = []
+    while (cnt-- > 0) {
+      const node = q.shift()
+      console.log(node)
+      temp.push(node[key])
+      if (node.children && node.children.length > 0) q.push(...node.children)
+    }
+    res.push(temp)
+  }
+  return res
+}
+
+const mockdata = [
+  {
+    name: 1,
+    children: [
+      {
+        name: 3,
+        children: [
+          {
+            name: 6,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 2,
+    children: [
+      {
+        name: 4,
+      },
+      {
+        name: 5,
+      },
+    ],
+  },
+]
+// 构造出[[1,2],[3,4,5],[6]]
+//构造一个虚拟节点，层序遍历
+const root = {
+  name: -1,
+  children: mockdata
+}
+
+bfs(root)
