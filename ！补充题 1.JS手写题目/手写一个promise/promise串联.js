42d2db97fafbfdda5530e2456ee77e76a30dd5b3
dujuan <dujuan@bixin.cn>
2023-03-13 17:39:53 +0800

    feat: 序列化

--- ！补充题 1.JS手写题目/手写一个promise/promise串联.js
+++ ！补充题 1.JS手写题目/手写一个promise/promise串联.js
@@ -1,32 +1,18 @@
-var createPromise = (time) => () =>
-  new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("timein" + time);
-      resolve();
-    }, time * 1000);
-  });
-
-function serpromise(arr) {
-  arr.reduce((pre, next) => {
-    return pre.then(next).catch(next); //本质就是不停地调用then
-  }, Promise.resolve());
+const fn1 = function (args) {
+  console.log('fn1', args)
+  return Promise.resolve('111')
+}
+const fn2 = function (args) {
+  console.log('fn2', args)
+  return Promise.resolve('222')
+}
+const fn3 = function (args) {
+  console.log('fn3', args)
+  return Promise.resolve('333')
 }
 
-var arr = [
-  createPromise(2),
-  createPromise(1),
-  createPromise(3),
-  createPromise(4),
-  createPromise(5),
-];
-// Promise.resolve().then(createPromise(2)).then(createPromise(1))
-serpromise(arr);
+const serialPromises = function (promises) {
+  promises.reduce((prev, next) => prev.then(next), Promise.resolve())
+}
 
-//async
-var fn = async function (arr) {
-  for (let i = 0, len = arr.length; i < len; i++) {
-    var result = await arr[i];
-    console.log(result);
-  }
-};
-fn(arr);
+serialPromises([fn1, fn2, fn3])
