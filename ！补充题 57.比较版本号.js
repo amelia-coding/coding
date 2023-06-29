8768c737fca5bbbdf043252ef072f8fdd4b72996
dujuan <dujuan@bixin.cn>
2023-06-29 11:51:18 +0800

    feat: 57比较版本号

--- ！补充题 57.比较版本号.js
+++ ！补充题 57.比较版本号.js
@@ -8,3 +8,36 @@
   5. 例子，1.2.3 < 1.2.4 < 1.3.0-alpha.1 < 1.3.0-alpha.2 < 1.3.0-beta.1 < 1.3.0-rc.1 < 1.3.0
 */
 
+function compare(a, b) {
+  const [aMajor, aMinor, aPatch, aSuffix, aNum] = parseVersion(a)
+  const [bMajor, bMinor, bPatch, bSuffix, bNum] = parseVersion(b)
+
+  if (aMajor !== bMajor) {
+    return aMajor > bMajor ? 1 : -1
+  } else if (aMinor !== bMinor) {
+    return aMinor > bMinor ? 1 : -1
+  } else if (aPatch !== bPatch) {
+    return aPatch > bPatch ? 1 : -1
+  } else if (aSuffix !== bSuffix) {
+    if (aSuffix === 'rc') {
+      return 1
+    } else if (aSuffix === 'beta' && bSuffix !== 'rc') {
+      return 1
+    } else if (aSuffix === 'alpha' && bSuffix !== 'beta' && bSuffix !== 'rc') {
+      return 1
+    } else {
+      return -1
+    }
+  } else if (parseInt(aNum) !== parseInt(bNum)) {
+    return parseInt(aNum) > parseInt(bNum) ? 1 : -1
+  } else {
+    return 0
+  }
+}
+
+function parseVersion(version) {
+  const [versionPart, suffixPart] = version.split('-')
+  const [major, minor, patch] = versionPart.split('.').map(Number)
+  const [suffix, num] = suffixPart ? suffixPart.split('.') : ['', '0']
+  return [major, minor, patch, suffix, num]
+}
