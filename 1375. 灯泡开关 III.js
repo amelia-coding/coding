6333b6bf892af80a442556cb2318355d117ee97a
dujuan <dujuan@bixin.cn>
2023-10-26 01:03:26 +0800

    feat: 开关

--- 1375. 灯泡开关 III.js
+++ 1375. 灯泡开关 III.js
@@ -51,3 +51,5 @@ var numTimesAllBlue = function (light) {
   }
   return ans
 }
+
+
--- 15. 三数之和.js
+++ 15. 三数之和.js
@@ -5,33 +5,36 @@
  * @return {number[][]}
  */
 var threeSum = function (nums) {
-  return findItem(nums, 0);
-};
+  return findItem(nums, 0)
+}
 
 function findItem(nums, sum) {
-  let ans = [];
-  const len = nums.length;
-  if (nums == null || len < 3) return ans;
-  nums.sort((a, b) => a - b);
+  let ans = []
+  const len = nums.length
+  if (nums == null || len < 3) return ans
+  nums.sort((a, b) => a - b)
   for (let i = 0; i < len; i++) {
-    if (nums[i] > sum) break;
-    if (i > 0 && nums[i] === nums[i - 1]) continue; //去重
+    if (nums[i] > sum) break
+    if (i > 0 && nums[i] === nums[i - 1]) continue //去重
     let right = len - 1,
-      left = i + 1;
+      left = i + 1
     while (left < right) {
-      const total = nums[i] + nums[left] + nums[right];
+      const total = nums[i] + nums[left] + nums[right]
       if (total < sum) {
-        left++;
+        left++
       } else if (total > sum) {
-        right--;
+        right--
       } else {
-        ans.push([nums[i], nums[left], nums[right]]);
-        while (left < right && nums[left] === nums[left + 1]) left++; //去重
-        while (left < right && nums[right] === nums[right - 1]) right--; //去重
-        left++;
-        right--;
+        ans.push([nums[i], nums[left], nums[right]])
+        while (left < right && nums[left] === nums[left + 1]) left++ //去重
+        while (left < right && nums[right] === nums[right - 1]) right-- //去重
+        console.log(left, right)
+        left++
+        right--
       }
     }
   }
-  return ans;
+  return ans
 }
+
+console.log(findItem([1, 1, 2, 3, 4, 5], 4))
