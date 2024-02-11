b227aa616081e2d0f3e9c9b4ed3d48fcfc9441d3
dujuan <dujuan@bixin.cn>
2024-02-11 19:03:56 +0800

    feat:  绳子

--- ！补充题 8.剑指offer/11. 旋转数组的最小数字.js
+++ ！补充题 8.剑指offer/11. 旋转数组的最小数字.js
@@ -1,6 +1,8 @@
 /*
 剑指 Offer 11. 旋转数组的最小数字
-把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。例如，数组 [3,4,5,1,2] 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。  
+把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。
+例如，数组 [3,4,5,1,2] 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。  
 
 示例 1：
 
@@ -17,18 +19,18 @@
  * @return {number}
  */
 var minArray = function (numbers) {
-  if (!numbers || numbers.length === 0) return 0;
+  if (!numbers || numbers.length === 0) return 0
   let left = 0,
-    right = numbers.length - 1;
+    right = numbers.length - 1
   while (left < right) {
-    let mid = left + Math.floor((right - left) / 2);
+    let mid = left + Math.floor((right - left) / 2)
     if (numbers[mid] > numbers[right]) {
-      left = mid + 1;
+      left = mid + 1
     } else if (numbers[mid] < numbers[right]) {
-      right = mid;
+      right = mid
     } else if (numbers[mid] === numbers[right]) {
-      right--;
+      right--
     }
   }
-  return numbers[right];
-};
+  return numbers[right]
+}
